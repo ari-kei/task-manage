6770401f0d32f7ef27dae733360b5a47e163d2b0
arikei <a.ri.777baseball@gmail.com>
2024-06-11 20:05:09 +0900

    トークンジェネレータをdomain層へ移動

--- auth/src/main/java/com/arikei/auth/domains/JwtGenerator.java
+++ auth/src/main/java/com/arikei/auth/domains/JwtGenerator.java
@@ -1,4 +1,4 @@
-package com.arikei.auth.domains.util;
+package com.arikei.auth.domains;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,27 +11,30 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import com.arikei.auth.domains.entities.AuthInfo;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 
 public class JwtGenerator {
 
   private Algorithm alg;
+  // jwtの有効期限(単位：分)
+  private final int EXPIRED_AT = 60;
 
   public JwtGenerator() {
-    PrivateKey PrivateKey = getPrivateKey();
+    PrivateKey privateKey = getPrivateKey();
     // TODO 環境変数から秘密鍵の場所を取得して、読み込み
     this.alg = Algorithm.RSA256();
   }
 
-  public String generateJwt(String userId, String role) {
+  public String generateJwt(AuthInfo authInfo) {
     return JWT.create()
         .withIssuer("TaskManagerAuth")
-        .withSubject(userId)
-        .withExpiresAt(OffsetDateTime.now().plusMinutes(60).toInstant())
+        .withSubject(authInfo.getUserId())
+        .withExpiresAt(OffsetDateTime.now().plusMinutes(this.EXPIRED_AT).toInstant())
         .withIssuedAt(OffsetDateTime.now().toInstant())
         .withJWTId(UUID.randomUUID().toString())
-        .withClaim("role", role)
+        .withClaim("role", authInfo.getRole())
         .sign(alg);
   }
 
