534d0b8337207da8e3c99527057c944acd6ed9ea
arikei <a.ri.777baseball@gmail.com>
2024-06-11 19:44:04 +0900

    パスワードエンコーダーをdomains配下に移動

--- auth/src/main/java/com/arikei/auth/domains/PasswordEncoder.java
+++ auth/src/main/java/com/arikei/auth/domains/PasswordEncoder.java
@@ -1,4 +1,4 @@
-package com.arikei.auth.domains.util;
+package com.arikei.auth.configures;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
