eec88962e17e616592960ededa335ed02493aaf4
arikei <a.ri.777baseball@gmail.com>
2024-06-20 18:54:57 +0900

    JWT生成検証処理修正

--- auth/src/main/java/com/arikei/auth/domains/JwtGenerator.java
+++ auth/src/main/java/com/arikei/auth/domains/JwtGenerator.java
@@ -2,67 +2,102 @@ package com.arikei.auth.domains;
 
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAKey;
 import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.time.OffsetDateTime;
 import java.util.Base64;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
 import com.arikei.auth.domains.entities.AuthInfo;
-import com.arikei.auth.domains.repositoryif.JwtPrivateKeyRepositoryIF;
+import com.arikei.auth.domains.repositoryif.JwtKeyRepositoryIF;
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 public class JwtGenerator {
 
-  private JwtPrivateKeyRepositoryIF jwtPrivateKeyRepositoryIF;
+  private JwtKeyRepositoryIF jwtKeyRepositoryIF;
+  private Algorithm alg;
 
   // jwtの有効期限(単位：分)
+  private final String ISSUER = "TaskManagerAuth";
   private final int EXPIRED_AT = 60;
   private final String BEGIN_RSA_PRIVATE_KEY = "-----BEGIN RSA PRIVATE KEY-----";
   private final String END_RSA_PRIVATE_KEY = "-----END RSA PRIVATE KEY-----";
+  private final String BEGIN_RSA_PUBLIC_KEY = "-----BEGIN RSA PUBLIC KEY-----";
+  private final String END_RSA_PUBLIC_KEY = "-----END RSA PUBLIC KEY-----";
+  private final String PRIVATE_MAP_KEY = "private";
+  private final String PUBLIC_MAP_KEY = "public";
 
-  public JwtGenerator(JwtPrivateKeyRepositoryIF jwtPrivateKeyRepositoryIF) {
-    this.jwtPrivateKeyRepositoryIF = jwtPrivateKeyRepositoryIF;
-  }
+  public JwtGenerator(JwtKeyRepositoryIF jwtKeyRepositoryIF) {
+    this.jwtKeyRepositoryIF = jwtKeyRepositoryIF;
 
-  public String generateJwt(AuthInfo authInfo) {
-    Optional<RSAPrivateKey> privatekeyOptional = getPrivateKey();
-    if (privatekeyOptional.isEmpty()) {
+    Optional<Map<String, RSAKey>> keyMapOptional = getKeyPair();
+    if (keyMapOptional.isEmpty()) {
       // TODO 鍵が取得できなかった際の処理
     }
-    return JWT.create()
-        .withIssuer("TaskManagerAuth")
-        .withSubject(authInfo.userId())
-        .withExpiresAt(OffsetDateTime.now().plusMinutes(this.EXPIRED_AT).toInstant())
-        .withIssuedAt(OffsetDateTime.now().toInstant())
-        .withJWTId(UUID.randomUUID().toString())
-        .withClaim("role", authInfo.role())
-        .sign(Algorithm.RSA256(privatekeyOptional.get()));
+    this.alg = Algorithm.RSA256((RSAPublicKey) keyMapOptional.get().get(PUBLIC_MAP_KEY),
+        (RSAPrivateKey) keyMapOptional.get().get(PRIVATE_MAP_KEY));
   }
 
-  private Optional<RSAPrivateKey> getPrivateKey() {
-    List<String> privateKeyLine = jwtPrivateKeyRepositoryIF.get();
+  /**
+   * JWTを生成する
+   * 
+   * @param authInfo
+   * @return
+   */
+  public String generateJwt(AuthInfo authInfo) {
+    return JWT.create().withIssuer(this.ISSUER).withSubject(authInfo.userId())
+        .withExpiresAt(OffsetDateTime.now().plusMinutes(this.EXPIRED_AT).toInstant())
+        .withIssuedAt(OffsetDateTime.now().toInstant()).withJWTId(UUID.randomUUID().toString())
+        .withClaim("role", authInfo.role()).sign(this.alg);
+  }
 
-    StringBuilder pemBuilder = new StringBuilder();
-    for (String line : privateKeyLine) {
-      if (this.BEGIN_RSA_PRIVATE_KEY.equals(line) || this.END_RSA_PRIVATE_KEY.equals(line)) {
-        continue;
-      }
-      pemBuilder.append(line);
+  public DecodedJWT verifyToken(String token) {
+    JWTVerifier verifier = JWT.require(this.alg).withIssuer(this.ISSUER).acceptExpiresAt(5).build();
+    try {
+      return verifier.verify(token);
+    } catch (JWTVerificationException e) {
+      System.out.println("JWT verification failed..");
+      throw e;
     }
+  }
+
+  private Optional<Map<String, RSAKey>> getKeyPair() {
+    // 秘密鍵取得
+    String privateKey = jwtKeyRepositoryIF.getPrivateKey();
+    String privateKeyTrimmed =
+        privateKey.replace(this.BEGIN_RSA_PRIVATE_KEY, "").replace(this.END_RSA_PRIVATE_KEY, "");
+
+    // 公開鍵取得
+    String publicKey = jwtKeyRepositoryIF.getPublicKey();
+    String publicKeyTrimmed =
+        publicKey.replace(this.BEGIN_RSA_PUBLIC_KEY, "").replace(this.END_RSA_PUBLIC_KEY, "");
     RSAPrivateKey rsaPrivateKey = null;
+    RSAPublicKey rsaPublicKey = null;
     try {
       rsaPrivateKey = (RSAPrivateKey) KeyFactory.getInstance("RSA")
-          .generatePrivate(new PKCS8EncodedKeySpec(Base64.getDecoder().decode(pemBuilder.toString())));
+          .generatePrivate(new PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKeyTrimmed)));
+
+      rsaPublicKey = (RSAPublicKey) KeyFactory.getInstance("RSA")
+          .generatePublic(new X509EncodedKeySpec(Base64.getDecoder().decode(publicKeyTrimmed)));
     } catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
       return Optional.empty();
     }
-    return Optional.of(rsaPrivateKey);
+
+    Map<String, RSAKey> keyMap = new HashMap<>();
+    keyMap.put(PRIVATE_MAP_KEY, rsaPrivateKey);
+    keyMap.put(PUBLIC_MAP_KEY, rsaPublicKey);
+    return Optional.of(keyMap);
   }
 }
