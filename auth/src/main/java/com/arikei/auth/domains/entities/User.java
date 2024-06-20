6e80795153750ae72b704021b3d02e4b519d59ff
arikei <a.ri.777baseball@gmail.com>
2024-06-20 19:56:17 +0900

    コンストラクタを設定

--- auth/src/main/java/com/arikei/auth/domains/entities/User.java
+++ auth/src/main/java/com/arikei/auth/domains/entities/User.java
@@ -1,10 +1,12 @@
 package com.arikei.auth.domains.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
  * ユーザエンティティ
  */
+@AllArgsConstructor
 @Getter
 public class User {
   private String userId;
