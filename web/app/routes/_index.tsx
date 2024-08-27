b58a2b9354c18eda88a7b9edb2db284bb888c7c6
arikei <a.ri.777baseball@gmail.com>
2024-08-28 08:46:20 +0900

    ホーム画面のヘッダーのスタイル修正

--- web/app/routes/_index.tsx
+++ web/app/routes/_index.tsx
@@ -10,11 +10,17 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">タスク管理</h1>
-      <Form action="/board/new">
-        <button type="submit">ボード新規作成</button>
-      </Form>
-    </div>
+    <header className="absolute inset-x-0 top-0 z-50">
+      <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <div className="font-sans p-4 flex lg:flex-1">
+          <h1 className="text-3xl">タスク管理</h1>
+        </div>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <Form action="/board/new">
+            <button type="submit" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">ボード新規作成</button>
+          </Form>
+        </div>
+      </nav>
+    </header>
   );
 }
