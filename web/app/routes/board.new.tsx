2bd8040c3542c6a09be0ec84b89176b4f4254db7
arikei <a.ri.777baseball@gmail.com>
2024-08-28 15:29:27 +0900

    タスクボード新規作成画面のデザイン整理

--- web/app/routes/board.new.tsx
+++ web/app/routes/board.new.tsx
@@ -36,19 +36,23 @@ export const action = async ({
 export default function Index() {
   const actionData = useActionData<typeof action>();
   return (
-    <div>
-      <h1 className="text-3xl">タスクボード新規作成</h1>
-      <Form method="post">
+    <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="mx-auto max-w-2xl text-center">
+        <h1 className="text-3xl ">タスクボード新規作成</h1>
+      </div>
+      <Form method="post" className="mx-auto mt-16 max-w-xl sm:mt-20">
         <p>
 
-          <label htmlFor="name">ボード名称</label>
-          <input type="text" name="name" />
+          <label htmlFor="name" className="block text-sm font-semibold leading-6 text-gray-900">ボード名称</label>
+          <input type="text" name="name" className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           {actionData?.errors?.name ? (
             <em>{actionData?.errors.name}</em>
           ) : null}
         </p>
-        <button type="submit">保存</button>
 
+        <div className="mt-10">
+          <button type="submit" className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">保存</button>
+        </div>
       </Form>
     </div>
   );
