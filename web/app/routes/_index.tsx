887bc27f5abf1f2daf2727298302f5cfbcae1901
arikei <a.ri.777baseball@gmail.com>
2024-08-28 15:21:41 +0900

    ヘッダーコンポーネントを配置

--- web/app/routes/_index.tsx
+++ web/app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
-import { Form } from "@remix-run/react";
+import { Form, Outlet } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,18 +9,5 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  return (
-    <header className="absolute inset-x-0 top-0 z-50">
-      <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-        <div className="font-sans p-4 flex lg:flex-1">
-          <h1 className="text-3xl">タスク管理</h1>
-        </div>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <Form action="/board/new">
-            <button type="submit" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">ボード新規作成</button>
-          </Form>
-        </div>
-      </nav>
-    </header>
-  );
+  return (<></>);
 }
