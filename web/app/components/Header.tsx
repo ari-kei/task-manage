d1a041660e782f6ab1d3b7dea70af5876284c87c
arikei <a.ri.777baseball@gmail.com>
2024-08-29 19:58:29 +0900

    Cookieのトークン有無によって、ログイン/ログアウトボタンを出し分けるように修正

--- web/app/components/Header.tsx
+++ web/app/components/Header.tsx
@@ -1,6 +1,6 @@
 import { Form } from "@remix-run/react"
 
-export default function Index() {
+export default function Index(props: { accessToken: string }) {
   return (
     <div>
       <header className="sticky inset-x-0 top-0 z-50">
@@ -9,9 +9,18 @@ export default function Index() {
             <a href="/" className="text-3xl">タスク管理</a>
           </div>
           <div className="mt-10 flex items-center justify-center gap-x-6">
-            <Form action="/login">
+            {/* <Form action="/login">
               <button type="submit" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">ログイン</button>
-            </Form>
+            </Form> */}
+            {props.accessToken ?
+              <Form action="/logout">
+                <button type="submit" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">ログアウト</button>
+              </Form>
+              :
+              <Form action="/login">
+                <button type="submit" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">ログイン</button>
+              </Form>
+            }
             <Form action="/board/new">
               <button type="submit" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">ボード新規作成</button>
             </Form>
--- web/app/root.tsx
+++ web/app/root.tsx
@@ -1,12 +1,16 @@
 import {
+  json,
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 import "./tailwind.css";
 import Header from "~/components/Header";
+import { LoaderFunctionArgs } from "@remix-run/node";
+import { getSession } from "./session";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -26,10 +30,26 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
+export const loader = async ({
+  request,
+}: LoaderFunctionArgs) => {
+  const session = await getSession(
+    request.headers.get("Cookie")
+  );
+  let accessToken = session.get("accessToken");
+  if (accessToken == undefined) {
+    accessToken = "";
+  }
+  console.log(accessToken);
+
+  return json({ accessToken });
+}
+
 export default function App() {
+  const { accessToken } = useLoaderData<typeof loader>();
   return (
     <>
-      <Header />
+      <Header accessToken={accessToken} />
       <Outlet />
     </>
   );
