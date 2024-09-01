e4b5f80651a0b16e72b99e9a3f57da84d5069687
arikei <a.ri.777baseball@gmail.com>
2024-09-01 19:32:29 +0900

    ボードの新規作成はボード一覧の最後のカードから行うように修正

--- web/app/routes/board._index.tsx
+++ web/app/routes/board._index.tsx
@@ -36,10 +36,13 @@ export default function Index() {
         {data?.boards?.map((board: { id: string, name: string }) => {
           return (
             <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{board.name}</h5>
+              <h5 className="mb-2 text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white">{board.name}</h5>
             </a>
           )
         })}
+        <a href="/board/new" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <h5 className="mb-2 text-4xl text-center font-bold tracking-tight text-gray-500 dark:text-white">+</h5>
+        </a>
       </div >
       <Outlet />
     </>
