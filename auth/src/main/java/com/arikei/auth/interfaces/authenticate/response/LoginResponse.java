f5040276d3b25a74efaa8143b6935c19f86ff162
arikei <a.ri.777baseball@gmail.com>
2024-09-03 14:59:57 +0900

    ログインエンドポイントの返り値エンティティからJsonPropertyアノテーション削除

--- auth/src/main/java/com/arikei/auth/interfaces/authenticate/response/LoginResponse.java
+++ auth/src/main/java/com/arikei/auth/interfaces/authenticate/response/LoginResponse.java
@@ -1,14 +1,11 @@
 package com.arikei.auth.interfaces.authenticate.response;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class LoginResponse {
-  @JsonProperty("token")
   private String accessToken;
-  @JsonProperty("error")
   private String error;
 }
