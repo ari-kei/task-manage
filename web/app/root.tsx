09dfdc307655d8268dabf7570a451484135ca565
arikei <a.ri.777baseball@gmail.com>
2024-09-28 17:58:52 +0900

    画面全体をスクロールしない設定を削除

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -14,7 +14,7 @@ import { getSession } from "./session";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ja" className="overflow-hidden">
+    <html lang="ja">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
