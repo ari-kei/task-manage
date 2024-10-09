e8a9ba53999b2abb9cb58d64a30abd0a52ff0ae3
arikei <a.ri.777baseball@gmail.com>
2024-10-10 08:04:19 +0900

    「/」のURLに遷移した際、アクセストークンの有無によって遷移先を振り分け

--- web/app/routes/_index.tsx
+++ web/app/routes/_index.tsx
@@ -1,4 +1,19 @@
-import type { MetaFunction } from "@remix-run/node";
+import { redirect, type LoaderFunctionArgs, type MetaFunction } from "@remix-run/node";
+import { getSession } from "~/session";
+
+export const loader = async ({
+  request
+}: LoaderFunctionArgs) => {
+  const session = await getSession(
+    request.headers.get("Cookie")
+  )
+  const accessToken = session.get("accessToken");
+  if (accessToken == undefined || accessToken.length <= 0) {
+    return redirect("/login");
+  }
+
+  return redirect("/board");
+}
 
 export const meta: MetaFunction = () => {
   return [
